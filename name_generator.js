943393c2fead9c369acce074ce4b5db5b47af867
Latrell Freeman <freemanlatrell@gmail.com>
2018-11-22 11:14:59 -0500

    Finally works!

--- name_generator.js
+++ name_generator.js
@@ -1,4 +1,4 @@
-const mailer = require('node-mailer');
+const nodemailer = require('nodemailer');
 
 let secret_santa_list = {
     "Latrell" : "freemanlatrell@gmail.com",
@@ -6,74 +6,57 @@ let secret_santa_list = {
     "Jaden": "jlfwedding@gmail.com"
 };
 
-
-function random(low, high, exclude_list, exclude_yourself) {
-
-    let list = [];
-    for (let i = low; i <= high; i++) {
-        list.push(i);
-    }
-
-    while (list.length > 0) {
-        let random_num = items[Math.floor(Math.random()*items.length)];
-    }
-
-    let num = Math.floor(Math.random() * (high - low + 1) + low);
-    let in_exclude_list = 0;
-    if (exclude_list.length > 0) {
-        for (let n in exclude_list) {
-            if (num === Number(n)) {
-                in_exclude_list++;
-            }
+let numbers = [0,1,2];
+var final_list = {};
+while (true) {
+    let names = Object.keys(secret_santa_list);
+    let reshuffle = 0;
+    final_list = {};
+    numbers = numbers.sort(function(a, b){return 0.5 - Math.random()});
+    for (let i = 0; i < names.length; i++) {
+        let selected_name = names[i];
+        let secret_santa = names[numbers[i]];
+        if (selected_name === secret_santa) {
+            reshuffle ++;
+            console.log('Trying again...');
+        } else {
+            final_list[selected_name] = secret_santa
         }
     }
 
-    if (num === exclude_yourself) {
-        in_exclude_list++;
-    }
-
-
-    //If generated number is in exclude list then generate a new number
-    if (in_exclude_list > 0) {
-        random(low, high, exclude_list, exclude_yourself)
-    } else {
-        return num;
+    if (reshuffle === 0) {
+        console.log(final_list);
+        break;
     }
 }
 
-let names = Object.keys(secret_santa_list);
 
-let selected = [];
+const EMAIL_ADD = '';
+const EMAIL_PASS = '';
 
-for (let i = 0; i < names.length; i++) {
 
-    let temp_name_list = Array.from(names);
-
-    //Remove yourself from temp list
-    temp_name_list.splice( temp_name_list.indexOf(names[i]), 1 );
-
-    //Remove selected names from temp list
-    for (let i = 0; i < selected.length; i++) {
-        temp_name_list.splice( temp_name_list.indexOf(selected[i]), 1 );
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: EMAIL_ADD,
+        pass: EMAIL_PASS
     }
+});
 
-    //Select random name from list
-    let random_name = temp_name_list[Math.floor(Math.random()*temp_name_list.length)];
-    selected.push(random_name);
+for (let names in final_list) {
 
-    console.log(random_name)
-    //let num = random(0, names.length - 1, my_santa, i);
-    //my_santa.push(num);
-    //console.log(names[i] + "'s secret santa is " + names[my_santa])
+    let mailOptions = {
+        from: EMAIL_ADD,
+        to: secret_santa_list[names],
+        subject: 'And your secret santa is...',
+        text: final_list[names]
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
-
-/*new mailer.Mail({
-    from: 'noreply@domain.com',
-    to: 'rodolphe@domain.com',
-    subject: 'My Subject',
-    body: 'My body',
-    callback: function(err, data){
-        console.log(err);
-        console.log(data);
-    }
-});*/
